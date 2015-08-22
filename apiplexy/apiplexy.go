cbff20fbbbcb775c25f0a2ee1d21e4da1bd51a67
Philipp Rustemeier <p.rustemeier@gmail.com>
2015-08-22 17:36:36 +0200

    go 1.5 update push

--- apiplexy/apiplexy.go
+++ apiplexy/apiplexy.go
@@ -1,8 +1,31 @@
 package main
 
 import (
-	"github.com/12foo/apiplexy"
+	"flag"
+	"fmt"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"os"
 )
 
 func main() {
+	configPath := flag.String("config", "$HOME/apiplexy-config.yaml", "Path to configuration file")
+	flag.Parse()
+	configFile := os.ExpandEnv(*configPath)
+	if _, err := os.Stat(configFile); os.IsNotExist(err) {
+		fmt.Println("Could not find configuration file.\n")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+	fmt.Printf("Using configuration file %s.\n", configFile)
+	fd, err := ioutil.ReadFile(configFile)
+	if err != nil {
+		fmt.Println("Could not read configuration file.")
+		os.Exit(1)
+	}
+	config := map[string]interface{}{}
+	if err := yaml.Unmarshal(fd, &config); err != nil {
+		fmt.Printf("Could not parse configuration file (error: %s).\n", err.Error())
+		os.Exit(1)
+	}
 }
